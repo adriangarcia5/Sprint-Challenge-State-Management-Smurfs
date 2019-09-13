7d613130c9081f6aacc273f3e909554251d0a906
Adrian-Garcia <agarc5@hotmail.com>
2019-09-13 11:22:31 -0500

    started 2 dispatch data/created action folder/file

--- smurfs/src/components/List.js
+++ smurfs/src/components/List.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Loader from 'react-loader-spinner' 
-import Smurfs from './Smurfs'
+import Smurfs from './Smurfs';
+import {getData} from '../action'
 
 const List = props => {
     return (<>
     <h1>Look at these Smurfs</h1>
     <h1>{props.test}
     </h1>
-    <button onClick={props.test}>
+    <button onClick={props.getData}>
         {props.isLoading ?
         <Loader
         type="Puff"
         color="#00BFFF"
         height="100"
-        width="100" /> : 'Get info'
+        width="100" /> : ('Get info')
         }
         </button>
         {props.webcams && props.chars.map(info => (
@@ -32,4 +33,4 @@ const mapStateToProps = state => {
   } ;
   
 
-export default connect(mapStateToProps,{})(List);
+export default connect(mapStateToProps,{getData})(List);
