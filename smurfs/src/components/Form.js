d6cea509d00e4e37d051daeae8411e7adc057df4
Adrian-Garcia <agarc5@hotmail.com>
2019-09-13 12:23:56 -0500

    add formik dependency/created form

---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = props => {
+    console.log('smurfs')
+    return (<>
+    <form>
+        <input></input>
+        <button>New Blue Thing</button>
+        </form>
+
+    </>
+    )}
+
+export default (Form); 
