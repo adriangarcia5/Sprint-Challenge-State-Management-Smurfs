5f7de641f8e4b354ec7c7641eea5c2372784becd
Adrian-Garcia <agarc5@hotmail.com>
2019-09-13 12:44:26 -0500

    form created/form finally functional/mvp done :)

--- smurfs/src/action/smurfsAction.js
+++ smurfs/src/action/smurfsAction.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 export const GET_SMURFS_DATA =  'GET_SMURFS_DATA';
 export const GET_SMURFS_SUCCESS =  'GET_SMURFS_SUCCESS';
 export const GET_SMURFS_FAILURE=  'GET_SMURFS_FAILURE';
+//=========
+export const SEND_SMURFS_DATA =  'SEND_SMURFS_DATA';
+export const SEND_SMURFS_SUCCESS =  'SEND_SMURFS_SUCCESS';
+export const SEND_SMURFS_FAILURE=  'SEND_SMURFS_FAILURE';
 
 export const getData = () => {
     return dispatch => {
@@ -14,5 +18,16 @@ export const getData = () => {
     }
         )
         .catch(err => {dispatch({type: GET_SMURFS_FAILURE, payload:  err.response})})
+    }
+}
+
+export const sendData = creds => {
+    return dispatch => {
+        axios.post('http://localhost:3333/smurfs', creds)
+        .then(res => {dispatch({type: GET_SMURFS_SUCCESS, payload:  res.data})
+        console.log(res)
+    }
+        )
+        .catch(err => console.log(err))
     }
 }
--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,11 +1,24 @@
-import {GET_SMURFS_DATA, GET_SMURFS_SUCCESS, GET_SMURFS_FAILURE} from '../action/smurfsAction'
-
+import {GET_SMURFS_DATA, GET_SMURFS_SUCCESS, GET_SMURFS_FAILURE, SEND_SMURFS_DATA, SEND_SMURFS_SUCCESS} from '../action/smurfsAction'
 
 const initialState = {
     chars: [], 
     test: 'Its working', 
     isLoading: false,
-    error: ''
+    error: '',
+    list: [
+        {
+          name: "Brainey",
+          age: 200,
+          height: "5cm",
+          id: 0
+        },
+        {
+          name: "Sleepy",
+          age: 200,
+          height: "5cm",
+          id: 1
+        }
+      ]
 }
 
 export const reducer = (state = initialState, action) => {
@@ -24,6 +37,21 @@ console.log(initialState.chars, 'chars')
                 chars: action.payload,
                 error: ''
             };
+            
+            case 'SEND_SMURFS_DATA':
+            return{
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+
+            case 'SEND_SMURFS_SUCCESS':
+            return{
+                ...state,
+                isLoading: true,
+                chars: action.payload,
+                error: ''
+            }
             
         default:
             return state;
